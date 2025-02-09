9f1e0b20bdb151ea2e70bf5728db6ce762025638
abdullah-tanriverdi <4abdullah.tanriverdi@gmail.com>
2025-02-09 23:23:59 +0300

    Profile Screen Geliştirildi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.androidx.runtime.livedata)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -76,6 +77,9 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:1.5.1")
 
     implementation ("androidx.navigation:navigation-compose:2.5.0")
+
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
 
 
 
--- app/src/main/java/com/tbox/jotter/profile/ProfileScreen.kt
+++ app/src/main/java/com/tbox/jotter/profile/ProfileScreen.kt
@@ -2,22 +2,24 @@ package com.tbox.jotter.profile
 
 
 import android.annotation.SuppressLint
+import android.net.Uri
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ArrowBack
@@ -29,6 +31,7 @@ import androidx.compose.material.icons.filled.Save
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -38,7 +41,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -49,15 +51,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import coil.compose.AsyncImage
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.firestore
+import com.google.firebase.storage.storage
 import com.tbox.jotter.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -65,23 +70,34 @@ import com.tbox.jotter.R
 @Composable
 fun ProfileScreen(navController: NavController) {
 
+
     val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
 
-    var firstName by remember { mutableStateOf("") }
-    var lastName by remember { mutableStateOf("") }
+    var name by remember { mutableStateOf("") }
+
     var phoneNumber by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var bio by remember { mutableStateOf("") }
 
     val firestore = Firebase.firestore
+    val storage = Firebase.storage
+
     var showMenu by remember { mutableStateOf(false) }
-    var profileImage by remember { mutableStateOf(R.drawable.jotter_unbackground) }
 val auth = Firebase.auth
+
     var isEditing by remember { mutableStateOf(false) }
+
+    var isLoading by remember { mutableStateOf(true) }
+
+    var profileImageUrl by remember { mutableStateOf<String?>(null) }
 
 
     val userId = auth.currentUser?.uid
+
+
+
     fun loadUserData(uid :String){
+        isLoading = true
         firestore.collection("users")
             .document(uid)
             .collection("profile")
@@ -89,26 +105,29 @@ val auth = Firebase.auth
             .get()
             .addOnSuccessListener { document ->
                 if (document != null && document.exists()) {
-                    firstName = document.getString("firstName") ?: ""
-                    lastName = document.getString("lastName") ?: ""
+                    name = document.getString("name") ?: ""
+
                     phoneNumber = document.getString("phoneNumber") ?: ""
                     email = document.getString("email") ?: ""
                     bio = document.getString("bio") ?: ""
+                    profileImageUrl = document.getString("profileImageUrl")
                 }
+                isLoading = false
             }
             .addOnFailureListener{
                 e->
+                isLoading = false
                 print("Error")
             }
     }
 
     fun saveUserData(uid: String){
         val user = hashMapOf(
-            "firstName" to firstName,
-            "lastName" to lastName,
+            "name" to name,
             "phoneNumber" to phoneNumber,
             "bio" to bio,
-            "email" to email
+            "email" to email,
+            "profileImageUrl" to profileImageUrl
         )
 
 
@@ -124,13 +143,88 @@ val auth = Firebase.auth
     }
 
 
+    fun uploadProfileImage ( uri: Uri){
+
+       // profileImageUrl = uri.toString()
+        userId?.let{
+            uid->
+            val storageRef = storage.reference.child("profile_images/$uid.jpg")
+            storageRef.putFile(uri)
+                .addOnSuccessListener {
+                    storageRef.downloadUrl.addOnSuccessListener {
+                        downloadUri ->
+                        profileImageUrl = downloadUri.toString()
+                        saveUserData(uid)
+                    }
+                }
+        }
+    }
+
+
+    fun deleteProfileImage() {
+        userId?.let { uid ->
+            val storageRef = storage.reference.child("profile_images/$uid.jpg")
+            storageRef.delete()
+                .addOnSuccessListener {
+
+                    val defaultImageUrl = "https://github.com/abdullah-tanriverdi/JotterApp/raw/master/app/src/main/res/drawable/jotter_unbackground.png"
+                    // Update the profileImageUrl in Firestore to null (or remove it)
+                    val userRef = firestore.collection("users")
+                        .document(uid)
+                        .collection("profile")
+                        .document("profile_data")
+
+                    userRef.update("profileImageUrl", defaultImageUrl)
+                        .addOnSuccessListener {
+                            // After updating Firestore, set profileImageUrl to null in the UI state
+                            profileImageUrl = defaultImageUrl
+                            saveUserData(uid) // Save the updated profile data without the image URL
+                        }
+                        .addOnFailureListener { e ->
+                            // Handle error in Firestore update
+                            println("Error updating Firestore: $e")
+                        }
+                }
+                .addOnFailureListener { e ->
+                    // Handle error in Firebase Storage delete
+                    println("Error deleting image: $e")
+                }
+        }
+    }
+
+    val imagePickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) {
+            uri: Uri? ->
+        uri?.let {
+            uploadProfileImage(it)
+        }
+    }
+
+
+
+
+LaunchedEffect(profileImageUrl) {
+    profileImageUrl?.let {
+        userId?.let {
+            uid ->
+            saveUserData(uid)
+        }
+    }
+}
+
+
+
     LaunchedEffect(userId) {
         userId?.let { loadUserData(it) }
     }
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Profile", color = MaterialTheme.colorScheme.onPrimary) },
+                title = {
+                    Text(
+                        if (isEditing) "Edit Profile" else "Profile",
+                       color = MaterialTheme.colorScheme.onPrimary) },
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
                         Icon(Icons.Filled.ArrowBack, contentDescription = "Back", tint = MaterialTheme.colorScheme.onPrimary)
@@ -211,109 +305,134 @@ val auth = Firebase.auth
 
         content = { paddingValues ->
 
-
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxSize()
-            ) {
-
-                Spacer(modifier = Modifier.height(40.dp))
-
-
+            if (isLoading) {
                 Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(200.dp),
+                    modifier = Modifier.fillMaxSize(),
                     contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator() // Yükleniyor göstergesi
+                }
+            } else {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxSize()
+
                 ) {
 
 
-                    Image(
-                        painter = painterResource(id = R.drawable.jotter_unbackground), // Placeholder image
-                        contentDescription = "Profile Picture",
+                        Spacer(modifier = Modifier.height(40.dp))
+
+
+
+
+                    Box(
                         modifier = Modifier
-                            .size(120.dp)
-                            .clickable { showMenu = !showMenu/* Handle profile photo click */ }
-                            .background(MaterialTheme.colorScheme.primary, CircleShape)
-                    )
-                    DropdownMenu(
-                        expanded = showMenu,
-                        onDismissRequest = { showMenu = false },
-                        modifier = Modifier.align(Alignment.Center),
+                            .fillMaxWidth()
+                            .height(200.dp),
+                        contentAlignment = Alignment.Center
                     ) {
-                        DropdownMenuItem(
-                            text = { Text("Fotoğraf Yükle") },
-                            onClick = {
 
-                            }
-                        )
-                        DropdownMenuItem(
-                            text = { Text("Fotoğrafı Sil") },
-                            onClick = {
+                        if (profileImageUrl != null) {
+                            AsyncImage(
+                                model = profileImageUrl,
+                                contentDescription = "Profile Picture",
+                                modifier = Modifier
+                                    .size(120.dp)
+                                    .clickable { showMenu = !showMenu
 
-                            }
-                        )
+                                    }
+                                    .background(Color.Magenta, CircleShape),
+                                contentScale = ContentScale.Crop
+                            )
+                        } else {
+                            Image(
+                                painter = painterResource(id = R.drawable.jotter_unbackground),
+                                contentDescription = "Profile Picture",
+                                modifier = Modifier
+                                    .size(120.dp)
+                                    .clickable { showMenu = !showMenu
+                                   }
+                                    .background(Color.Gray, CircleShape)
+                            )
+                        }
+                        DropdownMenu(
+                            expanded = showMenu,
+                            onDismissRequest = { showMenu = false },
+                            modifier = Modifier.align(Alignment.Center),
+                        ) {
+                            DropdownMenuItem(
+                                text = { Text("Fotoğraf Yükle") },
+                                onClick = {
+                                    showMenu = false
+                                    imagePickerLauncher.launch("image/*")
+                                }
+                            )
+                            DropdownMenuItem(
+                                text = { Text("Fotoğrafı Sil") },
+                                onClick = {
+                                    showMenu = false
+                                    deleteProfileImage()
+
+                                }
+                            )
+                        }
+
                     }
 
+                    Spacer(modifier = Modifier.height(10.dp))
+
+
+                    // First Name (Editable based on isEditing)
+                    OutlinedTextField(
+                        value = name,
+                        onValueChange = { if (isEditing) name = it },
+                        label = { Text("Name") },
+                        modifier = Modifier.fillMaxWidth(),
+                        readOnly = !isEditing
+                    )
+
+
+
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    // Phone Number (Editable based on isEditing)
+                    OutlinedTextField(
+                        value = phoneNumber,
+                        onValueChange = { if (isEditing) phoneNumber = it },
+                        label = { Text("Phone Number") },
+                        modifier = Modifier.fillMaxWidth(),
+                        readOnly = !isEditing
+                    )
+
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    // Email (Editable based on isEditing)
+                    OutlinedTextField(
+                        value = email,
+                        onValueChange = { if (isEditing) email = it },
+                        label = { Text("Email") },
+                        modifier = Modifier.fillMaxWidth(),
+                        readOnly = !isEditing
+                    )
+
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    // Bio (Editable based on isEditing)
+                    OutlinedTextField(
+                        value = bio,
+                        onValueChange = { if (isEditing && it.length<=75)
+                        {
+                            bio =it
+                        }},
+                        label = { Text("to myself") },
+                        modifier = Modifier.fillMaxWidth(),
+                        readOnly = !isEditing
+                    )
                 }
-
-                Spacer(modifier = Modifier.height(10.dp))
-
-
-                // First Name (Editable based on isEditing)
-                OutlinedTextField(
-                    value = firstName,
-                    onValueChange = { if (isEditing) firstName = it },
-                    label = { Text("First Name") },
-                    modifier = Modifier.fillMaxWidth(),
-                    readOnly = !isEditing
-                )
-
-                Spacer(modifier = Modifier.height(10.dp))
-
-                // Last Name (Editable based on isEditing)
-                OutlinedTextField(
-                    value = lastName,
-                    onValueChange = { if (isEditing) lastName = it },
-                    label = { Text("Last Name") },
-                    modifier = Modifier.fillMaxWidth(),
-                    readOnly = !isEditing
-                )
-
-                Spacer(modifier = Modifier.height(10.dp))
-
-                // Phone Number (Editable based on isEditing)
-                OutlinedTextField(
-                    value = phoneNumber,
-                    onValueChange = { if (isEditing) phoneNumber = it },
-                    label = { Text("Phone Number") },
-                    modifier = Modifier.fillMaxWidth(),
-                    readOnly = !isEditing
-                )
-
-                Spacer(modifier = Modifier.height(10.dp))
-
-                // Email (Editable based on isEditing)
-                OutlinedTextField(
-                    value = email,
-                    onValueChange = { if (isEditing) email = it },
-                    label = { Text("Email") },
-                    modifier = Modifier.fillMaxWidth(),
-                    readOnly = !isEditing
-                )
-
-                Spacer(modifier = Modifier.height(10.dp))
-
-                // Bio (Editable based on isEditing)
-                OutlinedTextField(
-                    value = bio,
-                    onValueChange = { if (isEditing) bio = it },
-                    label = { Text("Bio") },
-                    modifier = Modifier.fillMaxWidth(),
-                    readOnly = !isEditing
-                )
             }
+
+
         }
     )
 }
